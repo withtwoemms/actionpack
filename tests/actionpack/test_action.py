74bfdf7753105a4a13e18183172a75c7120b1c2a
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-01-31 13:57:54 -0600

    supports python >= 3.5

--- tests/actionpack/test_action.py
+++ tests/actionpack/test_action.py
@@ -11,11 +11,11 @@ class ActionTest(TestCase):
     def test_Action_produces_Right_result_when_performed(self):
         result = FakeAction().perform()
         self.assertIsInstance(result, Right)
-        self.assertEqual(result._value, FakeAction.result)
+        self.assertEqual(result.value, FakeAction.result)
 
     def test_Action_produces_Left_if_exception_raised_when_performed(self):
         exception = Exception('something went wrong :/')
         result = FakeAction(exception=exception).perform()
         self.assertIsInstance(result, Left)
-        self.assertEqual(result._error, exception)
+        self.assertEqual(result.value, exception)
 

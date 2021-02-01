b146e36fda827317a09b3d627b248a06d2ba7668
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-01-31 19:07:03 -0600

    adds ReadBytes

---
+++ actionpack/actions/read_bytes.py
@@ -0,0 +1,16 @@
+from actionpack import Action
+from pathlib import Path
+
+
+class ReadBytes(Action):
+    def __init__(self, filename: str):
+        self.path = Path(filename)
+        self.instruction = lambda: self.path.read_bytes()
+
+    def validate(self):
+        if not self.path.exists():
+            raise FileNotFoundError(str(self.path))
+        if self.path.is_dir():
+            raise IsADirectoryError(str(self.path))
+        return self
+

ab763a412be5908a72c19967c0ba75aab9651b19
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-02-02 21:05:53 -0600

    adds overwrite option to WriteBytes

--- tests/actionpack/actions/test_write_bytes.py
+++ tests/actionpack/actions/test_write_bytes.py
@@ -9,15 +9,18 @@ from unittest.mock import patch
 class WriteBytesTest(TestCase):
 
     class FakeFile:
-        def __init__(self, contents: bytes=bytes()):
+        def __init__(self, contents: bytes=bytes(), mode: str=None):
             self.buffer = BytesIO(contents)
             self.buffer.read()
+            self.mode = mode
 
         def read(self):
             self.buffer.seek(0)
             return self.buffer.read()
 
         def write(self, data: bytes):
+            if self.mode == 'wb':
+                self.buffer.seek(0)
             self.buffer.write(data)
             return len(data)
 
@@ -38,5 +41,18 @@ class WriteBytesTest(TestCase):
         result = action.perform()
 
         self.assertEqual(file.read(), salutation + question)
+        self.assertEqual(result.value, len(question))
+
+    @patch('pathlib.Path.open')
+    def test_can_overWriteBytes(self, mock_output):
+        salutation = 'Hello.'.encode()
+        question = ' How are you?'.encode()
+
+        file = self.FakeFile(salutation, 'wb')
+        mock_output.return_value = file
+        action = WriteBytes('valid/path/to/file', question, overwrite=True)
+        result = action.perform()
+
+        self.assertEqual(file.read(), question)
         self.assertEqual(result.value, len(question))
 

db523a375cd259a8f8e15d53b18d53350a29a777
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-02-02 23:33:58 -0600

    adds RetryPolicy

--- actionpack/actions/__init__.py
+++ actionpack/actions/__init__.py
@@ -1,6 +1,7 @@
 from actionpack.actions.make_request import MakeRequest
 from actionpack.actions.read_bytes import ReadBytes
 from actionpack.actions.read_input import ReadInput
+from actionpack.actions.retry_policy import RetryPolicy
 from actionpack.actions.write_bytes import WriteBytes
 
 
@@ -8,6 +9,7 @@ __all__ = [
     'MakeRequest',
     'ReadBytes',
     'ReadInput',
+    'RetryPolicy',
     'WriteBytes'
 ]
 

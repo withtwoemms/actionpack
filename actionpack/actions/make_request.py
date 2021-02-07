a1847ec2d5b24c3351eadd61bea1f865f1acf58c
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-02-06 19:30:31 -0600

    MakeRequest can be pickled

--- actionpack/actions/make_request.py
+++ actionpack/actions/make_request.py
@@ -13,8 +13,11 @@ class MakeRequest(Action):
     def __init__(self, method: str, url: str, data: dict=None, headers: dict=None, session: Session=None):
         self.method = method.upper()
         self.url = url
-        request = Request(method, self.url, data=data, headers=headers).prepare()
-        self.instruction = lambda: (session if session else Session()).send(request)
+        self.request = Request(method, self.url, data=data, headers=headers).prepare()
+        self.session = session
+
+    def instruction(self):
+        return (self.session if self.session else Session()).send(self.request)
 
     def validate(self):
         if self.method not in self.methods:

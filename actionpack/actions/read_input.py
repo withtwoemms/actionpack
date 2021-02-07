622946dc594ea7ece8968f82a32429e1fe1355df
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-02-06 19:22:44 -0600

    ReadInput can be pickled

--- actionpack/actions/read_input.py
+++ actionpack/actions/read_input.py
@@ -3,5 +3,8 @@ from actionpack import Action
 
 class ReadInput(Action):
     def __init__(self, prompt: str):
-        self.instruction = lambda: input(prompt)
+        self.prompt = prompt
+
+    def instruction(self) -> str:
+        return input(self.prompt)
 

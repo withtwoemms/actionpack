caaa0b24ff586b16781493ea5ed50056aab5d75b
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-05-18 23:10:37 -0500

    lint actionpack.procedure

--- actionpack/procedure.py
+++ actionpack/procedure.py
@@ -3,7 +3,6 @@ from actionpack import Action
 from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import as_completed
 from functools import reduce
-from multiprocessing.pool import ThreadPool
 
 
 class Procedure:
@@ -20,7 +19,7 @@ class Procedure:
         except Exception as e:
             raise e
 
-    def execute(self, max_workers: int=5, should_raise: bool=False, synchronously: bool=True):
+    def execute(self, max_workers: int = 5, should_raise: bool = False, synchronously: bool = True):
         if synchronously:
             for action in self.actions:
                 yield action.perform(should_raise=should_raise) if should_raise else action.perform()
@@ -32,7 +31,7 @@ class Procedure:
 
     def __repr__(self):
         for action in self.actions:
-            header = f'\nProcedure for performing the following Actions:\n'
+            header = '\nProcedure for performing the following Actions:\n'
             bullet = '  * '
             actions = reduce(lambda a, b: str(a) + f'\n{bullet}' + str(b), self.actions)
             return header + bullet + actions
@@ -46,7 +45,8 @@ class Procedure:
         except StopIteration:
             self._actions = iter(self.actions)
 
-    class NotAnAction(Exception): pass
+    class NotAnAction(Exception):
+        pass
 
 
 class KeyedProcedure(Procedure):
@@ -61,7 +61,7 @@ class KeyedProcedure(Procedure):
                 msg = f'All {self.__class__.__name__} Actions must have a name: {str(action)}'
                 raise KeyedProcedure.UnnamedAction(msg)
 
-    def execute(self, max_workers: int=5, should_raise: bool=False, synchronously: bool=True):
+    def execute(self, max_workers: int = 5, should_raise: bool = False, synchronously: bool = True):
         if synchronously:
             for action in self.actions:
                 yield (action.name, action.perform(should_raise=should_raise)) \
@@ -72,4 +72,5 @@ class KeyedProcedure(Procedure):
                 for future in as_completed(futures):
                     yield (futures[future].name, future.result())
 
-    class UnnamedAction(Exception): pass
+    class UnnamedAction(Exception):
+        pass

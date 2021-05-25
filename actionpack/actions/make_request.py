91ea34a8c0dc6b84230a703b11c3225c4e153ab4
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-05-25 01:16:54 -0500

    MakeRequests returns generics in absence of actual types

--- actionpack/actions/make_request.py
+++ actionpack/actions/make_request.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+from typing import TypeVar
 from validators import url as is_url
 
 from actionpack import Action
@@ -5,22 +7,27 @@ from actionpack.action import K
 from actionpack.action import T
 
 
+Session = TypeVar('Session')
+Request = TypeVar('Request')
+Response = TypeVar('Response')
+
+
 class MakeRequest(Action[T, K], requires=('requests',)):
 
     methods = ('CONNECT', 'GET', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH', 'POST', 'PUT', 'TRACE')
 
-    def __init__(self, method: str, url: str, data: dict = None, headers: dict = None, session: 'Session' = None):
+    def __init__(self, method: str, url: str, data: dict = None, headers: dict = None, session: Session = None):
         self.method = method.upper()
         self.url = url
         self.method = method
         self.data = data
         self.headers = headers
-        self.session: 'Session' = session
+        self.session = session
 
-    def prepare(self, method: str, url: str, data: dict = None, headers=None) -> 'Request':
+    def prepare(self, method: str, url: str, data: dict = None, headers=None) -> Request:
         return self.requests.Request(method, self.url, data=data, headers=headers).prepare()
 
-    def instruction(self) -> 'Response':
+    def instruction(self) -> Response:
         request = self.prepare(self.method, self.url, self.data, self.headers)
         return (self.session if self.session else self.requests.Session()).send(request)
 

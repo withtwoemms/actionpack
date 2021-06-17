9b9bc7d64d31b63ce58cd875aaaf2a0ea801f122
Emmanuel I. Obi <withtwoemms@gmail.com>
2021-06-17 14:18:23 -0400

    WIP -- 'middle of ocean'

--- actionpack/actions/pipeline.py
+++ actionpack/actions/pipeline.py
@@ -22,66 +22,42 @@ class Pipeline(Action):
         return self.flush(self.action)
 
     # recursive
-    # def flush(self, keyed_result: dict = None):
     def flush(self, given_action: Action = None):
         next_action_type = next(self)
         if next_action_type:
-            # if keyed_result:
-            #     return action_type(**keyed_result).perform().value
-            next_action_type_params = dict(signature(next_action_type.__init__).parameters.items())
+            print('>>>', next_action_type)
+            # if next_action_type == FittingType:
+            #     print('SUP!')
+            if next_action_type.__name__ == 'Fitting':
+                next_action_type_params = next_action_type.keywords
+                print('PARAMS ->>', next_action_type_params)
+            else:
+                next_action_type_params = dict(signature(next_action_type.__init__).parameters.items())
+                print('PARAMS ->', next_action_type_params)
             next_action_type_params.pop('self', None)
-            # useful_params = {k: v for k, v in action_type_params if k != 'self'}
             next_action_type_params_keys = list(next_action_type_params.keys())
-            # first_key = first(next_action_type_params_keys)  # first is fine for now...
-            first_key = next_action_type_params_keys.pop(0)  # first is fine for now...
-            # kp = dict(KeyedProcedure(*(self.start.set(name=key),)).execute())
-            # kp = dict(KeyedProcedure(*(given_action.set(name=key),)).execute(should_raise=True))
-            kprs = dict(KeyedProcedure(*(given_action.set(name=first_key),)).execute())
-            kprs.update({k: None for k in next_action_type_params_keys})  # should allow control of None
-            print(kprs)
-            keyed_result = {k: r.value for k, r in kprs.items()}
-            print(f'KEYED_RESULT: {keyed_result}')
+            first_key = next_action_type_params_keys[0] 
+            # first_key = next_action_type_params_keys.pop(0)  # first is fine for now...
+            if given_action.__class__.__name__ == 'Fitting':
+                # kprs = {}
+                kprs = given_action.keywords
+            else:
+                kprs = dict(KeyedProcedure(*(given_action.set(name=first_key),)).execute())
+            print('PARAM_KEYS ->', next_action_type_params_keys)
+            print('KPRS ->', kprs)
+            # kprs.update({k: None for k in next_action_type_params_keys if k not in next_action_type_params})  # should allow control of None
+            kprs.update({k: None for k in next_action_type_params_keys if k not in kprs})  # should allow control of None
+            # kprs.update({k: None for k in next_action_type_params_keys if kprs.get(k, None) is None})  # should allow control of None
+            print('KPRS *->', kprs)
+            if given_action.__class__.__name__ == 'Fitting':
+                keyed_result = kprs
+            else:
+                keyed_result = {k: r.value for k, r in kprs.items()}
+            print('KEYED_RESULT ->', keyed_result)
             next_action = next_action_type(**keyed_result)
+            # next_action = next_action_type(**kprs)
+            print('<<<', next_action)
             return self.flush(next_action)
-        else:
-            return 'HELLO'
-        #     if given_action:
-        #         # return action_type(**keyed_result).perform().value
-        #         pass
-        #     else:
-        #         # # TODO (withtwoemms) -- set the NAME of the action
-        #         # action_type_params = dict(signature(action_type.__init__).parameters.items())
-        #         # action_type_params.pop('self', None)
-        #         # # useful_params = {k: v for k, v in action_type_params if k != 'self'}
-        #         # key = first(list(action_type_params.keys()))
-        #         # kp = dict(KeyedProcedure(*(self.start.set(name=key),)).execute())
-        #         # keyed_result = {k: r.value for k, r in kp.items()}
-        #         # action = action_type(**keyed_result)
-        #         # # action_result = dict(KeyedProcedure(action).execute())
-        #         # # return self.flush(keyed_result=keyed_result)
-        #         return self.flush(next_action)
-        # else:
-        #     # TODO (withtwoemms) -- reconsider this behavior. may want to return None
-        #     return keyed_result
-        # # atype = next(self)
-        # # if atype:
-        # #     if result:
-        # #         print('RESULT:', result)
-        # #         return self.flush(atype(**result).perform().value)
-        # #     else:
-        # #         return self.flush(atype(self.first_result.value).perform().value)
-        # #     #if result:
-        # #     #    if isinstance(result, BoundArguments):
-        # #     #        return self.flush(atype(*result, **result))
-        # #     #    else:
-        # #     #        return self.flush(atype(result).perform().value)
-        # #     #else:
-        # #     #    if isinstance(result, BoundArguments):
-        # #     #        return self.flush(atype(*result, **result))
-        # #     #    else:
-        # #     #        return self.flush(atype(self.first_result.value).perform().value)
-        # # else:
-        # #     return result
 
     def __iter__(self):
         return self._action_types
@@ -96,34 +72,45 @@ class Pipeline(Action):
 class FittingType(type):
 
     @staticmethod
-    def init(instance, *args):
-        setattr(instance, 'args', args)
-        #for i, arg in enumerate(args):
-        #    setattr(instance, f'arg{i}', arg)
+    def init(instance, **kwargs):
+        print('INSTANCE', instance)
+        print('KWARGS', kwargs)
+        setattr(instance, 'keywords', kwargs)
+        # setattr(instance, 'kwargs', kwargs)
+
+    # @staticmethod
+    # def init(instance, *args):
+    #     print('INSTANCE', instance)
+    #     print('ARGS', args)
+    #     setattr(instance, 'args', args)
+    #     #for i, arg in enumerate(args):
+    #     #    setattr(instance, f'arg{i}', arg)
 
     @staticmethod
     def instruction(instance):
         # return dict(zip(instance.keywords, instance.args))
         instance.keyed_values
 
-    def __new__(mcs, keyed_results: Dict[str, Result]):
-        dct = {
-            '__init__': FittingType.init,
-            'instruction': FittingType.instruction,
-            'keyed_values': {k: r.value for k, r in keyed_results.items()}
-        }
-        cls = type('Fitting', (Action,), dct)
+    # def __new__(mcs, keyed_results: Dict[str, Result]):
+    #     dct = {
+    #         '__init__': FittingType.init,
+    #         'instruction': FittingType.instruction,
+    #         'keyed_values': {k: r.value for k, r in keyed_results.items()}
+    #     }
+    #     cls = type('Fitting', (Action,), dct)
 
-        return cls
+    #     return cls
 
     # def __new__(mcs, dct: dict = None, keywords: set = None):
-    #     dct = dct if isinstance(dct, dict) else dict()
-    #     dct['__init__'] = FittingType.init
-    #     dct['instruction'] = FittingType.instruction
-    #     keywords = keywords if isinstance(keywords, set) else set()
-    #     dct['keywords'] = keywords
-    #     cls = type('Fitting', (Action,), dct)
-    #     return cls
+    def __new__(mcs, dct: dict = None, keywords: dict = None):
+        dct = dct if isinstance(dct, dict) else dict()
+        dct['__init__'] = FittingType.init
+        dct['instruction'] = FittingType.instruction
+        # keywords = keywords if isinstance(keywords, set) else set()
+        keywords = keywords if isinstance(keywords, dict) else dict()
+        dct['keywords'] = keywords
+        cls = type('Fitting', (Action,), dct)
+        return cls
 
 
 class Fitting(Action):
--- tests/actionpack/actions/test_pipeline.py
+++ tests/actionpack/actions/test_pipeline.py
@@ -26,9 +26,9 @@ class PipelineTest(TestCase):
         #actions = [ReadBytes, ReadInput]
         #actions = [ReadBytes, ReadInput, Fitting(WriteBytes)]
         #def __init__(self, filename: str, bytes_to_write: bytes, overwrite: bool=False):
-        # action_types = [ReadBytes, ReadInput, FittingType(keywords={'bytes_to_write', filename}), WriteBytes]
+        action_types = [ReadBytes, ReadInput, FittingType(keywords={'bytes_to_write': filename}), WriteBytes]
         # action_types = [ReadBytes, ReadInput, FittingType, WriteBytes]
-        action_types = [ReadBytes, ReadInput, WriteBytes]
+        # action_types = [ReadBytes, ReadInput, WriteBytes]
         pipeline = Pipeline(read_input, *(action_type for action_type in action_types))
         result = pipeline.perform()
         print('RESULT ->', result)

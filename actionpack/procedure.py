9828e171c30eaa6975b1ab869cc7635390e6ac29
Emmanuel I. Obi <withtwoemms@gmail.com>
2022-01-19 11:36:48 -0600

    Procedure no longer accepts varargs

--- actionpack/procedure.py
+++ actionpack/procedure.py
@@ -1,8 +1,11 @@
 from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import as_completed
 from functools import reduce
+from itertools import islice
+from itertools import tee
 from typing import Generic
 from typing import Iterator
+from typing import Iterable
 from typing import Tuple
 
 from actionpack.action import Name
@@ -12,13 +15,17 @@ from actionpack import Action
 
 
 class Procedure(Generic[Name, Outcome]):
-    def __init__(self, *actions: Action[Name, Outcome]):
-        self.actions = actions
-        self._actions = iter(self.actions)
+    def __init__(self, actions: Iterable[Action[Name, Outcome]]):
+        if not (isinstance(actions, Iterator) or isinstance(actions, Iterable)):
+            raise TypeError(f'Actions must be iterable. Received {type(actions)}.')
+
+        self.actions, self._actions, self.__actions = tee(actions, 3)
 
     def validate(self):
+        actions, spare = tee(self.__actions, 2)
+        self.__actions = spare
         try:
-            for action in self.actions:
+            for action in actions:
                 if not isinstance(action, Action):
                     msg = f'Procedures can only execute Actions: {str(action)}'
                     raise Procedure.NotAnAction(msg)
@@ -32,7 +39,7 @@ class Procedure(Generic[Name, Outcome]):
         synchronously: bool = True
     ) -> Iterator[Result[Outcome]]:
         if synchronously:
-            for action in self.actions:
+            for action in self:
                 yield action.perform(should_raise=should_raise) if should_raise else action.perform()
         else:
             with ThreadPoolExecutor(max_workers=max_workers) as executor:
@@ -53,7 +60,7 @@ class Procedure(Generic[Name, Outcome]):
         try:
             return next(self._actions)
         except StopIteration:
-            self._actions = iter(self.actions)
+            return None
 
     class NotAnAction(Exception):
         pass
@@ -63,10 +70,15 @@ class KeyedProcedure(Procedure[Name, Outcome]):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        actions = list(args)[0]
+        self.actions, self._actions, self.__actions = tee(actions, 3)
+
         self.validate()
 
     def validate(self):
-        for action in self.actions:
+        actions, spare = tee(self.__actions, 2)
+        self.__actions = spare
+        for action in actions:
             if action.name is None:
                 msg = f'All {self.__class__.__name__} Actions must have a name: {str(action)}'
                 raise KeyedProcedure.UnnamedAction(msg)
@@ -78,7 +90,7 @@ class KeyedProcedure(Procedure[Name, Outcome]):
         synchronously: bool = True
     ) -> Iterator[Tuple[Name, Result[Outcome]]]:
         if synchronously:
-            for action in self.actions:
+            for action in self:
                 yield (action.name, action.perform(should_raise=should_raise)) \
                       if should_raise else (action.name, action.perform())
         else:
